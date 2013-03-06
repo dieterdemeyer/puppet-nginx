62cc235e93d4ca70ce902d8d02b10abaab3c1241
fdammeke <fabian.dammekens@cegeka.be>
2013-03-06 16:02:03 +0100

    Global nginx site settings

---
+++ manifests/site/settings.pp
@@ -0,0 +1,23 @@
+## define nginx::server
+define nginx::site::settings($ensure=present, $site=undef, $settings=undef) {
+  include nginx::params
+
+  if ! $site {
+    fail("Nginx::Site::Settings[site]:
+      parameter must be defined")
+  }
+  if ! $location {
+    fail("Nginx::Site::Settings[settings]:
+      parameter must be defined")
+  }
+  if $name !~ /^[a-zA-Z][a-zA-Z0-9_-]*$/ {
+    fail("Nginx::Site::Settings[${name}]:
+      parameter must be alphanumeric")
+  }
+
+  file { "${nginx::params::nginx_sites_enabled}/${site}.d/${name}.conf":
+    ensure  => present,
+    content => template('nginx/settings.erb'),
+    notify  => Service['nginx'],
+  }
+}
